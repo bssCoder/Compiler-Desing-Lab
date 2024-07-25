27684d5dacf7d03331913def6db074924a257a27
Bhawani Shankar Sarswat <130194346+bssCoder@users.noreply.github.com>
2024-07-25 10:59:40 +0530

    Add files via upload

---
+++ lab3_temp.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//	this is single line  comment 
+	cout<<"Welcome to LAB 3\n";
+/* this is 
+	multiline comment
+	so what can you do Ashok
+	*/	
+	int y=13;
+	cout<<y;
+	/* 
+	cout<<100110<<"\nDone"; */ 
+	
+	return 0;
+	
+}
