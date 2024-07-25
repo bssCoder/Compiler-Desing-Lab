adf53624fb23350574b4447835aed1d9fdaef74a
Bhawani Shankar Sarswat <130194346+bssCoder@users.noreply.github.com>
2024-07-25 11:16:11 +0530

    Create Comment Remover.cpp

---
+++ Lab 1/Comment Remover.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define OUT 0
+#define IN 1
+string removeComments(string prgm)
+{
+	
+	int n = prgm.length();
+	string res;
+	bool s_cmt = false;
+	bool m_cmt = false;
+	for (int i=0; i<n; i++)
+	{
+		if (s_cmt == true && prgm[i] == '\n')
+			s_cmt = false;
+		else if (m_cmt == true && prgm[i] == '*' && prgm[i+1] == '/')
+			m_cmt = false, i++;
+		else if (s_cmt || m_cmt)
+			continue;
+		else if (prgm[i] == '/' && prgm[i+1] == '/')
+			s_cmt = true, i++;
+		else if (prgm[i] == '/' && prgm[i+1] == '*')
+			m_cmt = true, i++;
+		else res += prgm[i];
+	}
+	return res;
+}
+string readFileToString(const string& filename) {
+    ifstream file(filename.c_str());
+    if (!file.is_open()) {
+        cerr << "Error opening file: " << filename << '\n';
+        return "";
+    }
+    string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+    file.close();
+    return content;
+}
+void writeStringToFile(const string& filename, const string& content) {
+    ofstream file(filename.c_str());
+    if (!file.is_open()) {
+        cerr << "Error creating or opening file: " << filename << endl;
+        return;
+    }
+    file << content;
+    file.close();
+    cout << "Content written to: " << filename << '\n'<<"Content is \n"<<content;
+}
+int main() {
+//	input cpp file path
+    string filename = "C:\\Users\\students\\Desktop\\lab3_temp.cpp";
+    string fileContent = readFileToString(filename);
+//    output cpp file path
+    string outputFilename = "C:\\Users\\students\\Desktop\\comment_output.cpp";
+	cout << "Given Program \n";
+	cout << filename << "\n";
+	if (!fileContent.empty()) {
+        cout << "File content:" << "\n";
+        cout << fileContent << "\n";
+    }
+	cout << "\nModified Program \n";
+	fileContent=removeComments(fileContent);
+	writeStringToFile(outputFilename, fileContent);
+}
